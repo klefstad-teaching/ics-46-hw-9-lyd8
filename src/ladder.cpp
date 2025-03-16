6bf4eb1e030fd99740dd6224cf9e4fcab734b6cf
lyd8 <lyd8@uci.edu>
2025-03-16 10:24:18 +0000

    .

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -127,10 +127,9 @@ void print_word_ladder(const vector<string>& ladder) {
         cout << "No word ladder found." << endl;
         return;
     }
-    for(size_t i = 0; i < ladder.size(); i++) {
-        cout << ladder[i];
-        if(i < ladder.size() - 1)
-            cout << " ";
+    cout << "Word ladder found: ";
+    for (size_t i = 0; i < ladder.size(); i++) {
+        cout << ladder[i] << " ";
     }
     cout << endl;
 }
