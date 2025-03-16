7a271eb3eb582194e36d89d2ac51cfdaa19b8faf
lyd8 <lyd8@uci.edu>
2025-03-16 01:12:42 +0000

    .

---
+++ src/ladder.cpp
@@ -0,0 +1,30 @@
+#include "ladder.h"
+#include <algorithm>
+using namespace std;
+
+void error(string word1, string word2, string msg) {
+
+}
+
+bool edit_distance_within(const string& s, const string& t, int d) {
+    
+}
+
+bool is_adjacent(const string& word1, const string& word2) {
+
+}
+
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+
+}
+  
+void load_words(set<string> & word_list, const string& file_name) {
+
+}
+
+void print_word_ladder(const vector<string>& ladder) {
+
+}
+void verify_word_ladder() {
+
+}
